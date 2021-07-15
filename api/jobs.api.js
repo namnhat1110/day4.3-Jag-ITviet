e75aeb538b9b570b1b7ffc7a0cf58ddd9b9f67fc
NhatNam <namnhat1110@gmail.com>
2021-07-15 16:16:27 +0700

    render and create new job

--- api/jobs.api.js
+++ api/jobs.api.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-let jobs = require('../data.json')
+let { jobs } = require('../data.json')
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.json(jobs)
+});
+
+router.post('/', function (req, res, next) {
+  const j = req.body;
+  jobs.unshift(j)
+  res.json(j)
+
 });
 
 module.exports = router;
--- script.js
+++ script.js
@@ -1,15 +1,75 @@
-async function getFoos() {
-  const response = await fetch("http://localhost:5000/api/foos");
-  const json = await response.json();
-  console.log(json);
+function renderJob(j) {
+  return `
+  <div class="col">
+            <div class="card shadow-sm">
+              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg"
+                role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                <title>${j.title}</title>
+                <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef"
+                  dy=".3em">Thumbnail</text>
+              </svg>
+              <div class="card-body">
+                <p class="card-text">${j.city}</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                  </div>
+                  <small class="text-muted">9 mins</small>
+                </div>
+              </div>
+            </div>
+          </div>
+  `
 }
 
-getFoos();
+let jobs = []
 
 async function getJobs() {
-  const response = await fetch("http://localhost:5000/api/jobs");
+  const response = await fetch("http://localhost:3000/api/jobs");
   const json = await response.json();
-  // console.log(json);
+  jobs = json
+  renderJobs()
+}
+getJobs();
+
+function renderJobs() {
+  const jobsHTML = []
+  for (const job of jobs) {
+    jobsHTML.push(renderJob(job))
+  }
+  document.getElementById('jobList').innerHTML = jobsHTML.join('')
 }
 
-getJobs();
+getJobs()
+
+
+const form = document.getElementById('jobCreate')
+
+form.addEventListener('submit', (e) => {
+  console.log({ e });
+  e.preventDefault();
+  const title = document.getElementById('title').value;
+  const city = document.getElementById('city').value;
+  createJob({ title, city })
+})
+
+async function createJob(j) {
+  const resp = await fetch('http://localhost:3000/api/jobs', {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify(j)
+  })
+  const json = await resp.json()
+  console.log({ json })
+  console.log({ jsonFoo: json })
+  jobs.unshift(json)
+  renderJobs()
+}
